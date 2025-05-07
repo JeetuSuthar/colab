5e5adabcbe605918e495dde667609199ddc19da5
jeetu-crafteak <jeetusuthar2315@gmail.com>
2025-05-08 00:23:49 +0530

    updates

--- py/dsbda6.py
+++ py/dsbda6.py
@@ -2,6 +2,8 @@
 
 import numpy as np
 import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import GaussianNB
 
 df=pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/iris-data.csv')
 df
@@ -13,10 +15,10 @@ df.isnull().sum()
 x=df[['sepal length',  'sepal width',  'petal length',  'petal width']]
 y=df['class']
 
-from sklearn.model_selection import train_test_split
+
 x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.2,random_state=42)
 
-from sklearn.naive_bayes import GaussianNB
+
 model=GaussianNB()
 model.fit(x_train,y_train)
 
